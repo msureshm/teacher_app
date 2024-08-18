1fd5846a524cc4e9c8abba16dabec8792a24ac9c
msureshm <sureshmurthygari@gmail.com>
2024-08-18 18:24:39 +0530

    Displaying students list

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+students = ["Alex Johnson", "Jamie Lee", "Taylor Smith", "Jordan Brown", "Casey Davis"]
+subjects = %w[
+Mathematics
+Biology
+History
+Computer Science
+Literature
+]
+(1..15).each do |i|
+  Student.create(name: students.sample, subject: subjects.sample, marks: rand(0..100))
+end
