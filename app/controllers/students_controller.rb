3e42a2c0714ccdfd490a2f36eedac96d9a164c52
msureshm <sureshmurthygari@gmail.com>
2024-08-19 10:57:15 +0530

    2024-08-19

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,6 +4,21 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      flash[:notice] = "Student was successfully updated"
+      redirect_to students_path
+    else
+      flash[:alert] = "Student could not be updated"
+      render :edit
+    end
+  end
+
   def destroy
     @student = Student.find(params[:id])
     if @student.destroy
@@ -13,5 +28,11 @@ class StudentsController < ApplicationController
       flash[:alert] = "Student could not be deleted"
       redirect_to root_path
     end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :subject, :marks)
   end
 end
