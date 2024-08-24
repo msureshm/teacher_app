9f19dbcab42517d809763ff0a75887173d48b7bd
msureshm <sureshmurthygari@gmail.com>
2024-08-24 17:31:41 +0530

    Add student modal CSS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+document.getElementById("openModal").onclick = function() {
+  document.getElementById("myModal").style.display = "block";
+};
+
+window.onclick = function(event) {
+  if (event.target == document.getElementById("myModal")) {
+      document.getElementById("myModal").style.display = "none";
+  }
+};
