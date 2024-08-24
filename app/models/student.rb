044053b181a6ba7bfa64530ae00360e274ad1bad
msureshm <sureshmurthygari@gmail.com>
2024-08-24 18:51:58 +0530

    Deleting student with new css, added validation for students fields presence

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,5 @@
 # student model
 class Student < ApplicationRecord
   validates :name, uniqueness: { scope: :subject, message: 'should be unique per subject' }
+  validates :name, :subject, :marks, presence: true
 end
